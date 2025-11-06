e5285dbcc0c4fd0f15918039d9cb1de861b4be15
siddiq24 <siddiqrm24@gmail.com>
2025-11-06 10:07:22 +0700

    doc(router): add route for swager

--- routers/init_router.go
+++ routers/init_router.go
@@ -3,9 +3,14 @@ package routers
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/siddiq24/golang-gin/models"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func InitRouter(r *gin.Engine, db *[]models.User) {
 	InitUserRouter(r, db)
 	InitAuthRouter(r, db)
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 }
